13ce196f49e9b520b6bba43669fe541378fe790c
zahrafeisal <zahrafeisal3@gmail.com>
2025-01-16 00:33:25 +0300

    classes and instances

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,15 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
     pass
+
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,9 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     pass
